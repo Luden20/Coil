a2a12e1e602eba2d46e4ad93bfa9cb26374333d8
Luden20 <aachafla@puce.edu.ec>
2023-10-26 14:53:16 -0500

    fix

--- parcial-2-a-o-2023-Luden20-master/main.cpp
+++ parcial-2-a-o-2023-Luden20-master/main.cpp
@@ -7,12 +7,10 @@ int main() {
     clock_t begin;
     std::cout << "Comenzando a medir Tiempo\n" << std::endl;
     begin = clock();
-    int i;
     std::string NombreArchivo = "Inventariado Fisico.csv";
     std::string NombreBusquedaStock = "ANGULOS FIXSYSTEM";
     ListaDatos DatosGuardados(NombreArchivo);
-    DatosGuardados.stock(NombreBusquedaStock,5);
-    Lista<std::string> Nombres=DatosGuardados.min_stock(2,1);
+    Lista<std::string> Nombres=DatosGuardados.min_stock(90);
     Nombres.print();    
     clock_t end = clock();
     double elapsed_secs = static_cast<double>(end - begin) / CLOCKS_PER_SEC;
