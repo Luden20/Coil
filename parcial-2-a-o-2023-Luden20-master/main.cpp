e967cf23416dd30b719df0d71af8c920e72cf7d5
Luden20 <aachafla@puce.edu.ec>
2023-10-31 19:05:25 -0500

    as

--- parcial-2-a-o-2023-Luden20-master/main.cpp
+++ parcial-2-a-o-2023-Luden20-master/main.cpp
@@ -10,72 +10,7 @@
 #include "Datos/ClaseDATO.h"
 #include <string>
 #include <functional>
-/*
-int ContarLineas(const std::string &nombrearchivo) {
-        std::string linea;
-        int i = 0;
-        std::ifstream archivo(nombrearchivo);
-        while (std::getline(archivo, linea)) {
-            i++;
-        }
-        archivo.close();
-        return i;
-}
-HashMap<std::string,Datos> CrearTabla(std::string NombreArchivo)
-{
-std::string linea, aux;
-        char l = ',';
-        int i=0;
-        ifstream archivo(NombreArchivo);
-        int dim = ContarLineas(NombreArchivo);
-        HashMap<string, Datos>tablaHash(dim);
-        std::getline(archivo, linea); // Read and discard the header line
-        while (getline(archivo, linea)) {
-            std::stringstream stream(linea);
-            std::string Grupo, CodigoBarras, Articulo, d1, d2, d3, d4, d5;
-            int D1 = 0, D2 = 0, D3 = 0, D4 = 0, D5 = 0;
-            std::getline(stream, Grupo, l);
-            if (Grupo != "") {
-                aux = Grupo;
-            }
-            Grupo = aux;
-            std::getline(stream, CodigoBarras, l);
-            std::getline(stream, Articulo, l);
-            std::getline(stream, d1, l);
-            std::getline(stream, d2, l);
-            std::getline(stream, d3, l);
-            std::getline(stream, d4, l);
-            std::getline(stream, d5, l);
-            if (!d1.empty()) {
-                D1 = std::stoi(d1);
-            }
-            if (!d2.empty()) {
-                D2 = std::stoi(d2);
-            }
-            if (!d3.empty()) {
-                D3 = std::stoi(d3);
-            }
-            if (!d4.empty()) {
-                D4 = std::stoi(d4);
-            }
-            if (!d5.empty()) {
-                D5 = std::stoi(d5);
-            }
-            Datos aux(Grupo, CodigoBarras, Articulo, D1, D2, D3, D4, D5);
-            tablaHash.put(Articulo, aux);
-            i++;
-        }
-         cout << "Se termino la creacion\n";
-         return tablaHash;
-    }
-    int stock(std::string nombre_articulo,HashMap<std::string,Datos>hash)
-    {
-        Datos aux=hash.get(nombre_articulo);
-        int stock = aux.getTotal();
-        cout<<"El total de "<<aux.getArticulo()<<" es "<<stock<<"\n";
-        return stock;
-    }
-*/
+
 int ContarLineas(const std::string &nombrearchivo) {
         std::string linea;
         int i = 0;
@@ -86,17 +21,20 @@ int ContarLineas(const std::string &nombrearchivo) {
         archivo.close();
         return i;
     }
-    unsigned int HashP(std::string st)
+unsigned int HashP(std::string st)
+{   
+    unsigned int clave = 0;
+    for (char c : st)
     {
-        unsigned int clave=0;
-        for(char c:st)
+        if (c != '(' && c != ')')
         {
-            if(c!='('||c!=')')
-                clave+=static_cast<unsigned int>(c);
+            clave += static_cast<unsigned int>(c);
         }
-        cout<<clave<<" "<<st<<"\n";
-        return clave;
     }
+    clave = clave % 232; // 231 + 1
+    cout << clave << " " << st << "\n";
+    return clave;
+}
 
 HashMap<int, Datos>ConstruirTabla(const std::string NombreArchivo)
      {
@@ -107,8 +45,10 @@ HashMap<int, Datos>ConstruirTabla(const std::string NombreArchivo)
         ifstream archivo(NombreArchivo);
         int dim = ContarLineas(NombreArchivo);
         HashMap<int, Datos>tablaHash(dim);
+        cout<<"Creando...\n";
         std::getline(archivo, linea); // Read and discard the header line
         while (getline(archivo, linea)) {
+            cout<<i<<"-";
             std::stringstream stream(linea);
             std::string Grupo, CodigoBarras, Articulo, d1, d2, d3, d4, d5;
             int D1 = 0, D2 = 0, D3 = 0, D4 = 0, D5 = 0;
@@ -138,11 +78,10 @@ HashMap<int, Datos>ConstruirTabla(const std::string NombreArchivo)
             }
             if (!d5.empty()) {
                 D5 = std::stoi(d5);
-            }
-            clave=HashP(Articulo);
+            };
             Datos aux(Grupo, CodigoBarras, Articulo, D1, D2, D3, D4, D5);
+            clave=HashP(Articulo);
             tablaHash.put(clave, aux);
-            i++;
         }
          cout << "Se termino la creacion\n";
          return tablaHash;
@@ -153,7 +92,7 @@ void Programa()
 }
 int main() {
     std::string NombreArchivo = "Inventariado Fisico.csv";
-    ListaDatos ListaD(NombreArchivo);
+    //ListaDatos ListaD(NombreArchivo);
     clock_t begin;
     std::cout << "Comenzando a medir Tiempo\n" << std::endl;
     begin = clock();
@@ -166,7 +105,7 @@ int main() {
     int xdasda=ListaD.total_art_dif();
     xd.print();
     */
-   HashMap<int, Datos>tahs=ConstruirTabla(NombreArchivo);
+    HashMap<int, Datos>tahs=ConstruirTabla(NombreArchivo);
     clock_t end = clock();
     double elapsed_secs = static_cast<double>(end - begin) / CLOCKS_PER_SEC;
     std::cout << "Tardó " << elapsed_secs << " segundos." << std::endl;
